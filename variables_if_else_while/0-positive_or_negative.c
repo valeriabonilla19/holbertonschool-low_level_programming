2319b52bb113b23927ebd3d9eae529b9d562f459
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-19 00:55:42 -0400

    task_0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ if (n > 0)
 {
 printf("%i is positive\n", n);
 }
- else if (n ==0)
-   {
-     printf("%i is zero\n", n);
-	  }
- else if (n < 0)
-   {
-     printf("%i is negative\n", n);
-   }
-	return (0);
+else if (n ==0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0);
 }
