d3e37d2b93f5c2e0be7b936b973d1fcc6eb44db4
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-19 11:38:23 -0400

    task_2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ * followed by a new line.
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
 char ch = 'a';
-while(ch <= 'z'){
+while (ch <= 'z')
+{
 putchar (ch);
 ch++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
