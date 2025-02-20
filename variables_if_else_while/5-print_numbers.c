90ecaedb979243ead19f94170a1e08689a87b987
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-20 10:10:26 -0400

    task_5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers
+* Descrption: of base starting from 0
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+
+printf("%d", a);
+
+putchar ('\n');
+
+return (0);
+}
