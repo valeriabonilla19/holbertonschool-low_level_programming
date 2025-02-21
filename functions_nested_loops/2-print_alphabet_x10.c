228247fa867f79a913eb15b0c9d054ee54bc7510
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-21 15:11:44 -0400

    task_2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print alphabet -  Write the alphabet 10 times
+* in lowercase
+*/
+
+void print_alphabet_x10(void)
+{
+  int i, l;
+for (i = 0; i < 10; i++)
+{
+for (l = 97; l <= 122; l++)
+_putchar(l);
+}
+ _putchar('\n');
+}
