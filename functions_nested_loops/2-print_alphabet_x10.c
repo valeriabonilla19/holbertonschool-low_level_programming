af2b44c9f80333f5d196d82df8f150b1656c14f3
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-23 22:39:05 -0400

    task_2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print alphabet -  Write the alphabet 10 times
-* in lowercase followed by a new line
+* print_alphabet_x10 -  Write the alphabet 10 times in lowercase,
+* followed by a new line
 */
 
 void print_alphabet_x10(void)
