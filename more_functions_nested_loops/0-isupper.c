7efa7c9beff1385e6fd6bfe18770e98a4488a8f5
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-25 10:04:32 -0400

    task_0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isupper - Checks for uppercase character.
+*@c: The character to check
+*
+*Description - Uses _putchar to display '1' for uppercase and '0' otherwisse
+*Return: 1 if c is uppercase, otherwise 0.
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90){
+return (1);
+}
+else{
+return (0);
+}
+}
