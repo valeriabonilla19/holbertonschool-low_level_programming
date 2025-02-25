0775cf77e4b08b5494288576ae1a9fce91fd693a
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-25 11:24:53 -0400

    task_3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - function that prints numbers
+*
+* Description - print numbers from 0 to 9
+*/
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
