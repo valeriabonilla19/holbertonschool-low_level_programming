a265c42b1176be0a2d60202f6781083fa80498be
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-26 14:59:35 -0400

    task_7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,9 @@ void print_diagonal(int n)
 int i, j;
 if (n > 0)
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0; j <= i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
@@ -20,5 +20,8 @@ _putchar('\\');
 _putchar('\n');
 }
 }
+else
+{
 _putchar('\n');
+}
 }
