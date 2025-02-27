1ffab8ffe0fd86d805b5b6aa937b843ed67a306b
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-02-27 09:57:17 -0400

    task_9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,22 @@
  * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
 {
   int i;
-  for (i = 1; i <= 100; i++)
+  for(i = 1; i <= 100; i++)
     {
-    if (i % 3 == 0 && i % 5 == 0)
+    if(i % 3 == 0 && i % 5 == 0)
       printf ("FizzBuzz");
-    else if (i % 3 == 0)
-      printf ("Fizz");
-    else if (i % 5 == 0)
-      printf ("Buzz");
+    else if(i % 3 == 0)
+      printf("Fizz");
+    else if(i % 5 == 0)
+      printf("Buzz");
     else
-      printf ("%d", i);
-  if (i != 100)
-    printf (" ");
+      printf("%d", i);
+  if(i != 100)
+    printf(" ");
 }
-printf ("\n");
-return (0);
+printf("\n");
+return(0);
 }
