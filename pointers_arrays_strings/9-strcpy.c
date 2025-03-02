b03caaac5966d94bf9cd6ede640f6bc329e8405e
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-03-01 22:06:33 -0400

    task_8

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strcpy - Copies a string from src to dest
+* @dest: The destination buffer
+* @src: The source string
+*
+* Return: Pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+if (src == NULL || dest == NULL)
+return (NULL);
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
