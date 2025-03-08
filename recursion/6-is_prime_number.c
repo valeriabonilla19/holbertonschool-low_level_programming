95fdbdccd05017cb5db891b13c77a439b2501aa6
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-03-08 00:19:50 -0400

    task_6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* is_prime_number - Function that returns 1 if the integer is a prime number
+* otherwise return 0.
+* @n: The integer to be checked
+*
+* Return: 1 if n is a prime number, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+int i;
+if (n <= 1)
+return (0);
+for (i = 2; i * i <= n; i++)
+{
+if (n % i == 0)
+return (0);
+}
+return (1);
+}
