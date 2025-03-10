d391a8b74fb1cb3a6327844aa1b60f9c5ec94355
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-03-09 21:03:25 -0400

    task_0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints the program's name
- * @argc: Argument count
- * @argv: Argument vector (array of strings)
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point, prints the program's name
+* @argc: Argument count
+* @argv: Argument vector (array of strings)
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-    (void)argc; /* Suppress unused variable warning */
-    printf("%s\n", argv[0]);
+(void)argc; /* Suppress unused variable warning */
+printf("%s\n", argv[0]);
 
-    return (0);
+return (0);
 }
