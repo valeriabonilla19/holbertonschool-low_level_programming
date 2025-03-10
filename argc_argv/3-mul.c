94ff6fdaaeaaf8a070844e2dbd179d227ad88999
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-03-09 21:11:05 -0400

    task_3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -20,7 +20,7 @@ return (1);
 
 num1 = atoi(argv[1]); /* Convert first argument to an int */
 num2 = atoi(argv[2]); /* Convert second argument to an int */
-result = num1 * num2;
+result = num1 *num2;
 
 printf("%d\n", result);
 
