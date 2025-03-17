b78f9fbffd03b0a5050b5a0260d3f085154cf5a8
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-03-16 22:18:41 -0400

    task_1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,11 +13,12 @@ char *name;
 float age;
 char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
 /**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
