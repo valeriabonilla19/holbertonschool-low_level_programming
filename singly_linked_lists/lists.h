b32252792489597980be4476f7471322dfb6b704
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-04-28 18:02:29 -0400

    task_1

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stddef.h> /* for size_t */
 
 /**
 * struct list_s - singly linked list
@@ -19,5 +19,6 @@ struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
