e6e897956de481765a818214c3543342fb080034
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-04-28 20:56:09 -0400

    task_5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* flip_bits - Returns the number of bits needed to flip to get from one number to another.
+* @n: The first number.
+* @m: The second number.
+*
+* Return: The number of bits you would need to flip to get from `n` to `m`.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int diff = n ^ m;  /* XOR the numbers to find differing bits */
+unsigned int count = 0;
+
+while (diff > 0)  /* Count the set bits in the result of XOR */
+{
+count += diff & 1;  /* Increment count if the least significant bit is 1 */
+diff >>= 1;  /* Shift the bits to the right to check the next bit */
+}
+
+return (count);
+}
